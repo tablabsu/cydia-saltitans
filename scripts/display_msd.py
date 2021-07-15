cf0c535190dc8b433b95142a041f02180c146cd9
Devon McKee <devon.l.mckee@gmail.com>
2021-07-15 13:12:53 -0700

    Adding multiple pos data chunk compatibility to display_msd

--- scripts/display_msd.py
+++ scripts/display_msd.py
@@ -1,6 +1,7 @@
 #! python3
 import sys, os, argparse, logging, json, math
 import numpy as np
+from random import random
 from matplotlib import pyplot as plt
 
 # --------- UTILITY METHODS --------- 
@@ -13,7 +14,7 @@ def dist(x1, y1, x2, y2):
 
 # Setting up argument parser
 parser = argparse.ArgumentParser(description="Display mean-squared displacement (MSD) plots from position data files")
-parser.add_argument("path", help="Path to file containing position data")
+parser.add_argument("path", nargs='+', help="Path to file containing position data")
 parser.add_argument("-d", "--debug", action="store_true", help="Show debug information")
 args = vars(parser.parse_args())
 
@@ -25,7 +26,7 @@ if args.get("debug"):
     logging.getLogger().setLevel(logging.DEBUG)
 logging.debug("ARGS: {0}".format(args))
 
-# Check that file at path exists and ends in .json
+'''# Check that file at path exists and ends in .json
 if not os.path.exists(args["path"]):
     logging.warning("Given path does not exist! Exiting...")
     sys.exit(1)
@@ -59,11 +60,97 @@ for obj in objects:
 
         # Average the displacement of those intervals
         msd = sum(tau_dist) / len(tau_dist)
+        #msd = tau_dist
 
         # Add to object's tau and msd list
         o['tau'].append(t)
         o['msd'].append(msd)
-    objects_msd.append(o)
+    objects_msd.append(o)'''
+
+# Calculate MSD data for each object in each file
+objects_msd = []
+for path in args['path']:
+    # Check that file at path exists and ends in .json
+    if not os.path.exists(path):
+        logging.warning("Given path does not exist! Exiting...")
+        sys.exit(1)
+    if not os.path.split(path)[-1].endswith(".json"):
+        logging.warning("Given path does not point to a .json file! Exiting...")
+        sys.exit(1)
+
+    objects = []
+    canvas = {}
+    # Read position data from file
+    logging.info("Reading position data from '{0}'...".format(os.path.split(path)[-1]))
+    with open(path) as fp:
+        data = json.load(fp)
+        objects = data['objects']
+        canvas = data['canvas']
+
+    logging.info("Calculating squared displacements from '{0}'".format(os.path.split(path)[-1]))
+
+    # Calculate squared displacements
+    for i, obj in enumerate(objects):
+        if len(objects_msd) < (i + 1):
+            # 'New' object
+            o = { 'tau': [], 'msd': [] }
+            tau = range(1, int(len(obj['X']) / 2))
+            for t in tau:
+                # Pull out X and Y coordinate lists
+                tau_x = obj['X']
+                tau_y = obj['Y']
+
+                # Calculate distance between points apart by interval tau, then square displacement
+                tau_dist = [dist(tau_x[i], tau_y[i], tau_x[i+t], tau_y[i+t]) for i in range(len(tau_x) - t)]
+                tau_dist = [math.pow(i, 2) for i in tau_dist]
+
+                # Average the displacement of those intervals
+                #msd = sum(tau_dist) / len(tau_dist)
+                msd = tau_dist
+
+                # Add to object's tau and msd list
+                o['tau'].append(t)
+                o['msd'].append(msd)
+            objects_msd.append(o)
+        else:
+            # Object seen in previous file
+            o = objects_msd[i]
+            tau = range(1, int(len(obj['X']) / 2))
+            for t in tau:
+                # Pull out X and Y coordinate lists
+                tau_x = obj['X']
+                tau_y = obj['Y']
+
+                # Calculate distance between points apart by interval tau, then square displacement
+                tau_dist = [dist(tau_x[i], tau_y[i], tau_x[i+t], tau_y[i+t]) for i in range(len(tau_x) - t)]
+                tau_dist = [math.pow(i, 2) for i in tau_dist]
+
+                # Average the displacement of those intervals
+                #msd = sum(tau_dist) / len(tau_dist)
+                msd = tau_dist
+
+                # Add to object's tau and msd list
+                o['tau'].append(t)
+                o['msd'].append(msd)
+            objects_msd[i] = o
+        
+
+# Merge tau entries for each object
+logging.info("Merging squared displacements into MSD...")
+for obj in objects_msd:
+    tau = []
+    msd = []
+    for t in range(1, max(obj['tau'])):
+        sd = []
+        for i, u in enumerate(obj['tau']):
+            if t == u:
+               sd.extend(obj['msd'][i]) 
+        if len(sd) > 0:
+            msd_val = sum(sd) / len(sd)
+            tau.append(t)
+            msd.append(msd_val)
+    obj['tau'] = tau
+    obj['msd'] = msd
 
 # Set up plots
 logging.info("Setting up plots...")
@@ -73,21 +160,24 @@ for i, obj in enumerate(objects_msd):
     tau = [math.log(i, 10) if i != 0 else i for i in obj['tau']]
     msd = [math.log(i, 10) if i != 0 else i for i in obj['msd']]
 
+    color = (random(), random(), random())
+
     # Plot scatter of log tau/msd
-    plt.scatter(tau, msd)
+    plt.scatter(tau, msd, color=color)
 
     # Plot trendlines of log tau/msd
     pf = np.polyfit(tau, msd, 1)
     p = np.poly1d(pf)
-    plt.plot(tau, p(tau), '--', label="Object {1} slope: {0:.2f}".format(pf[0], i))
+    plt.plot(tau, p(tau), '--', label="Object {1} slope: {0:.2f}".format(pf[0], i), color=color)
 
 plt.title("Log MSD over Log Tau")
 plt.xlabel("Log Tau")
 plt.ylabel("Log MSD ({0})".format(canvas['units'] + "^2"))
-plt.legend()
+if len(objects_msd) > 1:
+    plt.legend()
 plt.show()
 
 if input("Save figure? ").lower() in ('y', 'yes'):
     logging.info("Saving figure...")
     plt.tight_layout()
-    fig.savefig("figure", dpi=144)
+    fig.savefig("figure-msd", dpi=144)
